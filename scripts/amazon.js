81e5ef11d7c7d0708eb3a538a7aea438e668d9ec
aswin <raswin021@gmail.com>
2024-11-03 18:44:45 -0700

    Added Modules

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,4 @@
 let productsHTML = "";
-const products = [
-    {
-        image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating : {
-            stars : 4.5,
-            count : 87
-        },
-        price : 1090 
-    },
-    {
-        image : 'images/products/intermediate-composite-basketball.jpg',
-        name : 'Intermediate Size Basketball',
-        rating : {
-            stars : 4.0,
-            count : 127
-        },
-        price : 2095
-    },
-    {
-        image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating : {
-            stars : 4.5,
-            count : 56
-        },
-        price : 799
-    }
-];
-
 products.forEach( (product) => {
     productsHTML += `<div class="product-container">
           <div class="product-image-container">
@@ -49,7 +19,7 @@ products.forEach( (product) => {
           </div>
 
           <div class="product-price">
-            $${(product.price / 100).toFixed(2)} 
+            $${(product.priceCents / 100).toFixed(2)} 
           </div>
 
           <div class="product-quantity-container">
@@ -74,10 +44,36 @@ products.forEach( (product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id = "${product.id}">               
             Add to Cart
           </button>
         </div>`
 }
-);
+);//data attribute : data-<name> = "value"
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach(
+    (button) => {
+        button.addEventListener('click', () => {
+            const productId = button.dataset.productId;  //returns all data attributes (attributes taht  begin with data-). Give product name in camelCase
+            let matchingItem = undefined
+            cart.forEach((item) => {
+                if(item.productId === productId){
+                    matchingItem = item;
+                }
+            }
+            );
+            if(matchingItem){
+                matchingItem.quantity += 1;
+            }
+            else{
+                cart.push({
+                    productId : productId,
+                    quantity : 1
+                })
+            }
+            console.log(cart);
+        })
+    }
+);
